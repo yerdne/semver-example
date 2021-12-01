c3f5588174d9ec2855c5f7231b5eda960e5829f4
Endrey Diaz <ediazc@koandina.com>
2021-12-01 12:39:41 -0300

    Version 1.0.3

--- content.js
+++ content.js
@@ -1,2 +1,3 @@
-console.log('Feature for version 1.0.0, only for enterprises');
+console.log('Feature for version 1.0.3, only for enterprises');
+console.log('Fixing version');
 console.log('Message: if you are a person, it\'s locked! ')
